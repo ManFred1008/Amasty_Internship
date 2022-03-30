5122181b460915c339c6f4aa9bd52da30793d0f5
Andrey Baranowskiy <andrew.bar90@gmail.com>
2022-03-30 09:00:41 +0300

    Task3_done

--- MyModule/Block/Form.php
+++ MyModule/Block/Form.php
@@ -2,9 +2,45 @@
 
 namespace Amasty\MyModule\Block;
 
+use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\View\Element\Template;
 
 class Form extends Template
 {
+    /**
+     * @var ScopeConfigInterface
+     */
+    protected $scopeConfig;
 
+    public function __construct(
+        Template\Context $context,
+        ScopeConfigInterface $scopeConfig,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    /**
+     * Returns action url for contact form
+     *
+     * @return string
+     */
+
+    public function getFormAction()
+    {
+        return $this->getUrl('andrewbar/index/addtocart', ['_secure' => true]);
+    }
+
+    public function getQty()
+    {
+        return ($this->scopeConfig->isSetFlag('my_module_config/qty_settings/qty_enabled')) ?: false;
+    }
+
+    public function getQtyValue()
+    {
+        return $this->scopeConfig->getValue('my_module_config/qty_settings/default_qty') ?: 0;
+    }
 }
+
+
